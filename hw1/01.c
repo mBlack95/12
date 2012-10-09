34d3b8d424f848d0b8c44461d0d167ee16f1bd7d
mBlack <gbdrock@abv.bg>
2012-10-09 03:01:10 +0300

    hw1

---
+++ hw1/01.c
@@ -0,0 +1,21 @@
+//1. Да се разработи програма, която изисква от потребителя да въведе две целочислени числа, x и y, където x < y. Да се намери сумата на всички нечетни числа в интервала [x,y].
+
+#include<stdio.h>
+
+int main() {
+	int x, y;
+	printf("Vavedete x: ");
+	scanf("%d", &x);
+	printf("Vavedete y: ");
+	scanf("%d", &y);
+    while (x >= y) {
+	printf("Vavedete y: ");
+	scanf("%d", &y);
+	}
+
+    int t=0;
+    for( x; x<=y; x++) {
+        if (x%2 != 0) t=x+t;
+	}
+    printf("TOTAL: %d\n", t);
+}
---
+++ hw1/02.c
@@ -0,0 +1,21 @@
+//2. Да се разработи програма, която изисква от потребителя да въведе две целочислени числа, x и y, където x < y. Да се намери и изведе сумата на числатa делящи се на 17 в интервал [x, y].
+
+#include<stdio.h>
+
+int main() {
+	int x, y;
+	printf("Vavedete x: ");
+	scanf("%d", &x);
+	printf("Vavedete y: ");
+	scanf("%d", &y);
+    while (x >= y) {
+	printf("Vavedete y: ");
+	scanf("%d", &y);
+	}
+
+	int t=0;
+    for( x; x<=y; x++) {
+        if (x%17 == 0) t=x+t;
+	}
+    printf("TOTAL: %d\n", t);
+}
